2263cd0871eafd8aac9dfdd644f3047ca97147f2
SimpleFreak <andreyrazinwork@yandex.ru>
2026-09-19 15:46:27 +0500

    FreakSystemMonitor program

---
+++ app/run_program.cpp
@@ -0,0 +1,42 @@
+#include "app/includes/process.hpp"
+#include "app/includes/thread.hpp"
+#include "app/includes/window.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+int32_t WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
+                       int32_t numberCmdShow) {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    if (!AllocConsole()) {
+        return EXIT_FAILURE;
+    }
+
+    FILE* fDummy;
+    freopen_s(&fDummy, "CONOUT$", "w", stdout);
+    freopen_s(&fDummy, "CONOUT$", "w", stderr);
+    freopen_s(&fDummy, "CONIN$", "r", stdin);
+
+    Process process{};
+
+    DWORD processId{process.getProcessIdByName(L"FreakSystemMonitor.exe")};
+    std::cout << "Process " << process.getProcessName().c_str() << " with pid " << processId << '\n';
+    process.printListProcessThreads(processId);
+
+    std::cout << '\n';
+    process.setProcessName(L"FreakSystemMonitor.exe");
+    Thread::printAllThreads(process);
+
+    Window window{};
+
+    if (!window.createWindow()) {
+        exit(EXIT_FAILURE);
+    }
+
+    window.runWindow();
+
+    FreeConsole();
+    return EXIT_SUCCESS;
+}
---
+++ lib/containers/vector.hpp
@@ -0,0 +1,10 @@
+#ifndef __LIB_VECTOR_HPP__
+#define __LIB_VECTOR_HPP__
+
+namespace FLibContainers {
+    class FVector {
+
+    };
+}
+
+#endif
